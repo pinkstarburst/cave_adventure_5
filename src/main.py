95e24043976950913c70c75c7346d6f434282ff5
Jamie Raymond <eversobrainless@gmail.com>
2026-06-25 13:50:58 -0400

    Extra text added

--- src/main.py
+++ src/main.py
@@ -2,3 +2,6 @@ print("This is a new version of the code in cave_adventure_5/src/main.py")
 
 
 print("this is extra text that I added to the file ")
+
+
+print("this is extra text that I added to the file, from the extra-game-loop branch")
